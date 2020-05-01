08f58a635e85d7854771349f4a7c3f8f12a62907
Daniel Gobbi <bergholz.daniel@gmail.com>
2020-05-01 19:44:17 -0300

    Bug fix: import css on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Bem vindos ao meu site!</h1>
+      <p>Meu nome é Daniel Bergholz, me siga nas redes sociais!</p>
+      <a href="https://www.instagram.com/berg.dev/" target="_blank">
+        @berg.dev
+      </a>
+      <br />
+      <a
+        href="https://www.youtube.com/channel/UCnmbV9eyMwIl50Ji1ObFxqg/videos?view_as=subscriber"
+        target="_blank"
+      >
+        Daniel Berg
+      </a>
+    </>
   );
-}
+};
 
 export default App;
