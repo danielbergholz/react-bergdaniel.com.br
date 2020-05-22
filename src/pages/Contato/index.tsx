380f12c36845a436d2c8435e451ce87f15e8ceb6
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-05-22 19:25:33 -0300

    Feature: Contato (page) - created 'contact me' form

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -3,50 +3,70 @@ import React from 'react';
 import { FaGithub, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa';
 import { GrMail } from 'react-icons/gr';
 import FotoPerfil from '../../assets/foto_perfil.png';
-import { Container, SocialMedia, CopyToClipBoard } from './styles';
+import {
+  Container,
+  SocialMedia,
+  CopyToClipBoard,
+  Form,
+  Contact,
+} from './styles';
 
 const Contato: React.FC = () => {
   return (
     <Container>
-      <img src={FotoPerfil} alt="" />
-      <SocialMedia>
-        <CopyToClipBoard>
-          <GrMail size={30} color="#fff" />
-          <p id="clipboard">bergholz.daniel@gmail.com</p>
-        </CopyToClipBoard>
-        <a
-          href="https://www.linkedin.com/in/daniel-gobbi-bergholz-752379149"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin size={30} color="#fff" />
-          <p>Daniel Gobbi Bergholz</p>
-        </a>
-        <a
-          href="https://www.github.com/danielbergholz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub size={30} color="#fff" />
-          <p>danielbergholz</p>
-        </a>
-        <a
-          href="https://www.youtube.com/channel/UCnmbV9eyMwIl50Ji1ObFxqg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaYoutube size={30} color="#fff" />
-          <p>Daniel Berg</p>
-        </a>
-        <a
-          href="https://www.instagram.com/berg.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagram size={30} color="#fff" />
-          <p>@berg.dev</p>
-        </a>
-      </SocialMedia>
+      <Contact>
+        <img src={FotoPerfil} alt="" />
+        <SocialMedia>
+          <CopyToClipBoard>
+            <GrMail size={30} color="#fff" />
+            <p id="clipboard">bergholz.daniel@gmail.com</p>
+          </CopyToClipBoard>
+          <a
+            href="https://www.linkedin.com/in/daniel-gobbi-bergholz-752379149"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={30} color="#fff" />
+            <p>Daniel Gobbi Bergholz</p>
+          </a>
+          <a
+            href="https://www.github.com/danielbergholz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={30} color="#fff" />
+            <p>danielbergholz</p>
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCnmbV9eyMwIl50Ji1ObFxqg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube size={30} color="#fff" />
+            <p>Daniel Berg</p>
+          </a>
+          <a
+            href="https://www.instagram.com/berg.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={30} color="#fff" />
+            <p>@berg.dev</p>
+          </a>
+        </SocialMedia>
+      </Contact>
+      <Form name="contact" netlify>
+        <h1>Entre em contato</h1>
+        <input type="text" name="name" placeholder="Nome" />
+        <br />
+        <input type="email" name="email" placeholder="E-mail" />
+        <br />
+        <input type="text" name="subject" placeholder="Assunto" />
+        <br />
+        <textarea name="description" placeholder="Descrição" cols={120} />
+        <br />
+        <button type="submit">enviar</button>
+      </Form>
     </Container>
   );
 };
--- src/pages/Contato/styles.ts
+++ src/pages/Contato/styles.ts
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 
+interface FormProps {
+  netlify: boolean;
+}
+
 export const Container = styled.div`
   width: 80vw;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+export const Contact = styled.div`
   display: flex;
   place-content: center;
   margin-top: 60px;
@@ -46,4 +56,64 @@ export const CopyToClipBoard = styled.div`
   display: flex;
   align-items: center;
   color: #fff;
+`;
+
+export const Form = styled.form<FormProps>`
+  margin-top: 60px;
+
+  h1 {
+    margin-bottom: 15px;
+  }
+
+  input,
+  textarea {
+    width: 700px;
+    border: 0;
+    padding: 10px 15px;
+    border-radius: 6px;
+    background-color: #5c5c5c;
+    font-size: 16px;
+    color: #fff;
+  }
+
+  input::placeholder,
+  textarea::placeholder {
+    color: #ababab;
+  }
+
+  br + input {
+    margin-top: 5px;
+  }
+
+  textarea {
+    resize: none;
+    margin-top: 5px;
+    height: 200px;
+  }
+
+  button {
+    margin: 15px 0;
+    font-family: 'Anton', Helvetica, Arial;
+    text-transform: uppercase;
+    font-size: 20px;
+    border: 0;
+    border-radius: 6px;
+    color: #fff;
+    background-color: #bb9878;
+    padding: 6px 14px;
+  }
+
+  @media (max-width: 950px) {
+    input,
+    textarea {
+      width: 500px;
+    }
+  }
+
+  @media (max-width: 700px) {
+    input,
+    textarea {
+      width: 288px;
+    }
+  }
 `;
