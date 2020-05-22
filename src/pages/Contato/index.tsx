30eb17d975d7befcd40e3a1bcf741bfce8cb179b
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-05-22 17:59:22 -0300

    Interface: added contact information on 'Contato' page

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FaGithub, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa';
+import { GrMail } from 'react-icons/gr';
+import FotoPerfil from '../../assets/foto_perfil.png';
+import { Container, SocialMedia, CopyToClipBoard } from './styles';
 
-const Contato: React.FC = () => (
-  <Container>
-    <h1>Contato</h1>
-  </Container>
-);
+const Contato: React.FC = () => {
+  return (
+    <Container>
+      <img src={FotoPerfil} alt="" />
+      <SocialMedia>
+        <CopyToClipBoard>
+          <GrMail size={30} color="#fff" />
+          <p id="clipboard">bergholz.daniel@gmail.com</p>
+        </CopyToClipBoard>
+        <a
+          href="https://www.linkedin.com/in/daniel-gobbi-bergholz-752379149"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={30} color="#fff" />
+          <p>Daniel Gobbi Bergholz</p>
+        </a>
+        <a
+          href="https://www.github.com/danielbergholz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={30} color="#fff" />
+          <p>danielbergholz</p>
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCnmbV9eyMwIl50Ji1ObFxqg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaYoutube size={30} color="#fff" />
+          <p>Daniel Berg</p>
+        </a>
+        <a
+          href="https://www.instagram.com/berg.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram size={30} color="#fff" />
+          <p>@berg.dev</p>
+        </a>
+      </SocialMedia>
+    </Container>
+  );
+};
 
 export default Contato;
--- src/pages/Contato/styles.ts
+++ src/pages/Contato/styles.ts
@@ -1,3 +1,49 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 80vw;
+  margin: 0 auto;
+  display: flex;
+  place-content: center;
+  margin-top: 60px;
+
+  a {
+    margin-top: 6px;
+    display: flex;
+    align-items: center;
+    color: #fff;
+  }
+
+  p {
+    margin-left: 8px;
+    font-size: 24px;
+  }
+
+  img {
+    margin-right: 40px;
+  }
+
+  @media (max-width: 700px) {
+    img {
+      display: none;
+    }
+  }
+`;
+
+export const SocialMedia = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+
+  @media (max-width: 700px) {
+    p {
+      font-size: 20px;
+    }
+  }
+`;
+
+export const CopyToClipBoard = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+`;
