b5897153a08451d8ec49a4c5a685b5e2ede4d193
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-06-02 13:27:00 -0300

    Feature: Portfolio (page)

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,11 +1,67 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Project, Text } from './styles';
+import berg from '../../assets/berg.png';
+import astroMuri from '../../assets/astro_muri.png';
 
 const Portfolio: React.FC = () => {
   return (
     <Container>
-      <h1>Portfolio</h1>
+      <p>Experiência</p>
+      <strong>
+        - Graduação:
+        <span> Graduando em Engenharia de Redes de Comunicações na UnB</span>
+      </strong>
+      <strong>
+        - Estágio:
+        <span>
+          {' '}
+          6 meses de experiência em desenvolvimento mobile com React Native
+        </span>
+      </strong>
+
+      <p>Projetos</p>
+      <Project>
+        <img src={astroMuri} alt="Astro Muri" />
+        <Text>
+          <strong>
+            - Função:
+            <span> Desenvolvedor Frontend</span>
+          </strong>
+          <br />
+          <strong>
+            - Descrição:
+            <span>
+              {' '}
+              Ricardo Muri é um astrólogo famoso e influencer, e “Astro Muri” é
+              um aplicativo mobile híbrido que faz streaming do seu conteúdo. Eu
+              desenvolvi tudo relacionado à tela de login, o processo de
+              pagamento (usando o pagseguro), e conectei todas as páginas ao
+              backend (firebase).
+            </span>
+          </strong>
+        </Text>
+      </Project>
+      <Project>
+        <img src={berg} alt="BERG" />
+        <Text>
+          <strong>
+            - Função:
+            <span> Desenvolvedor Frontend</span>
+          </strong>
+          <br />
+          <strong>
+            - Descrição:
+            <span>
+              {' '}
+              bergdaniel.com.br é o site que você está acessando. Eu o criei do
+              completo zero, desde a concepção da arquitetura e navegação de
+              páginas no whimsical e o design no Figma até o desenvolvimento com
+              React.
+            </span>
+          </strong>
+        </Text>
+      </Project>
     </Container>
   );
 };
