893f42ecf4ef328922198ced4f6aed9793c61406
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-06-02 11:53:46 -0300

    Chore: persist chosed theme on localstorage

--- src/context/theme.tsx
+++ src/context/theme.tsx
@@ -8,10 +8,26 @@ interface ThemeContextData {
 const ThemeContext = createContext<ThemeContextData>({} as ThemeContextData);
 
 export const ThemeProvider: React.FC = ({ children }) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+  const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+    const chosedTheme = localStorage.getItem('@bergdaniel:theme');
+
+    if (!chosedTheme) {
+      localStorage.setItem('@bergdaniel:theme', 'dark');
+      return 'dark';
+    }
+
+    return chosedTheme as 'light' | 'dark';
+  });
 
   const changeTheme = useCallback(() => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    // setTheme(theme === 'dark' ? 'light' : 'dark');
+    if (theme === 'dark') {
+      setTheme('light');
+      localStorage.setItem('@bergdaniel:theme', 'light');
+    } else {
+      setTheme('dark');
+      localStorage.setItem('@bergdaniel:theme', 'dark');
+    }
   }, [theme]);
 
   return (
