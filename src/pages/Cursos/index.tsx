bad3cd95b2c2a98d70befb40e43967cdfbd291ab
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-06-02 16:34:43 -0300

    Feature: created static version of the page "cursos"

--- src/pages/Cursos/index.tsx
+++ src/pages/Cursos/index.tsx
@@ -1,11 +1,70 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Course, Thumbnail, CourseList, Gold } from './styles';
 
 const Cursos: React.FC = () => {
   return (
     <Container>
-      <h1>Cursos</h1>
+      <p>Cursos gratuitos</p>
+      <CourseList>
+        <Course>
+          <Thumbnail>
+            <img
+              src="https://i.ytimg.com/vi/pL9nX6Ac2Lc/mqdefault.jpg"
+              alt=""
+            />
+          </Thumbnail>
+          <span> Curso de Javascript</span>
+        </Course>
+        <Course>
+          <Thumbnail>
+            <img
+              src="https://i.ytimg.com/vi/VRR3V42EdSg/mqdefault.jpg"
+              alt=""
+            />
+          </Thumbnail>
+          <span> Curso de Terminal Linux</span>
+        </Course>
+        <Course>
+          <Thumbnail>
+            <img
+              src="https://i.ytimg.com/vi/ck7WWHaAgpU/mqdefault.jpg"
+              alt=""
+            />
+          </Thumbnail>
+          <span> Curso de Trello</span>
+        </Course>
+        <Course>
+          <Thumbnail>
+            <p> em breve</p>
+          </Thumbnail>
+          <span> Curso de CSS</span>
+        </Course>
+      </CourseList>
+
+      <p>
+        Cursos <Gold> premium</Gold>
+      </p>
+      <CourseList>
+        <Course>
+          <Thumbnail>
+            <p> em breve</p>
+          </Thumbnail>
+          <span> Curso de React</span>
+        </Course>
+        <Course>
+          <Thumbnail>
+            <p> em breve</p>
+          </Thumbnail>
+          <span> Curso de React Native</span>
+        </Course>
+        <Course>
+          <Thumbnail>
+            <p> em breve</p>
+          </Thumbnail>
+          <span> Curso de Node</span>
+        </Course>
+      </CourseList>
     </Container>
   );
 };
--- src/pages/Cursos/styles.ts
+++ src/pages/Cursos/styles.ts
@@ -1,6 +1,78 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 80vw;
+  margin: 0 auto;
   animation: loadAnimation;
   animation-duration: 500ms;
+
+  > p {
+    margin-top: 40px;
+    margin-bottom: 25px;
+  }
+
+  p {
+    font-family: 'Anton', Helvetica, serif;
+    font-size: 32px;
+    text-transform: uppercase;
+  }
+
+  @media (max-width: 900px) {
+    p {
+      text-align: center;
+    }
+  }
+
+  @media (max-width: 700px) {
+    width: 90vw;
+  }
+`;
+
+export const Thumbnail = styled.div`
+  height: 180px;
+  width: 320px;
+  background-color: #717171;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  cursor: pointer;
+
+  img {
+    border-radius: 5px;
+  }
+
+  > p {
+    color: #fff;
+  }
+`;
+
+export const CourseList = styled.div`
+  display: grid;
+  gap: 30px 0;
+  grid-template-columns: repeat(4, 1fr);
+
+  @media (max-width: 1720px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 1350px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
+`;
+
+export const Course = styled.div`
+  span {
+    font-size: 18px;
+  }
+`;
+
+export const Gold = styled.p`
+  display: inline;
+  color: #ebce00;
 `;
